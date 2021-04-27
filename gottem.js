0d8db8258e6621c9fc11310b4b865d68d4096af2
Aaron <aaron.awesome.kirk@gmail.com>
2021-04-26 23:24:42 -0700

    Projects stuff and small details

---
+++ gottem.js
@@ -0,0 +1,27 @@
+const gottem = document.querySelector("#gottem");
+const me = document.querySelector("#me");
+
+var toggledContact = false;
+
+me.addEventListener("click", (e) => {
+    if(toggledContact) return;
+    toggledContact = true;
+
+    me.style.transform = "rotateY(90deg)";
+
+    setTimeout(() => {
+        gottem.style.transform = "rotateY(180deg)";
+    }, 300);
+});
+
+gottem.addEventListener("click", (e) => {
+    console.log("yeet");
+    if(!toggledContact) return;
+    toggledContact = false;
+
+    gottem.style.transform = "rotateY(90deg)";
+
+    setTimeout(() => {
+        me.style.transform = "rotateY(180deg)";
+    }, 300);
+})
