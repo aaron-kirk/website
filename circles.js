c547d12163c3797a7d8ccb2063020d3e8ec4efb2
Aaron <aaron.awesome.kirk@gmail.com>
2021-04-29 14:55:16 -0700

    oops shouldve committed in stages O_O

--- circles.js
+++ circles.js
@@ -6,7 +6,14 @@ function wToH(width) {
     return height;
 }
 
-const numCircles = Math.ceil(4*Math.random()+3);
+function wToP(width) {
+    var w = window.innerWidth;
+    var pixels = Math.ceil(width*w/100);
+    return pixels;
+}
+
+const numCircles = 4;
+const edge = 11;
 let circles = [];
 let circleRefs = [];
 
@@ -16,7 +23,7 @@ for(i = 0; i < numCircles; i++) {
     loop1: while(true) {
         var passed = true;
 
-        circles[i] = [Math.ceil(70*Math.random()+15), Math.ceil(70*Math.random()+15), Math.ceil(8*Math.pow(Math.random(),3)) + 3];
+        circles[i] = [Math.ceil((100 - 2*edge)*Math.random()+edge), Math.ceil((100 - 2.2*wToH(edge + 2))*Math.random()+wToH(edge)), Math.ceil(6*Math.pow(Math.random(),3)) + 5];
 
         // Check other circle for intersection; regenerate if failed
         loop2: for(j = 0; j < i; j++) {
@@ -40,16 +47,30 @@ for(i = 0; i < numCircles; i++) {
         if(passed) break loop1;
     }
 
-    var circ = document.createElement("div");
-    circ.className = "circle";
-    circ.id=`c${i}`;
+    var box = document.querySelector(`#b${i}`);
+    var circ = document.querySelector(`#c${i}`);
+    box.style.width = `${circles[i][2]}vw`;
+    box.style.height = `${circles[i][2]}vw`;
+    box.style.left = `${circles[i][0]}vw`;
+    box.style.top = `${circles[i][1]}vh`;
+
     circ.style.width = `${circles[i][2]}vw`;
     circ.style.height = `${circles[i][2]}vw`;
-    circ.style.left = `${circles[i][0]}vw`;
-    circ.style.top = `${circles[i][1]}vh`;
 
-    var delay = 2*Math.random();
+    if(wToP(circles[i][0]) < 500) {
+        var panel = document.querySelector(`#c${i} .info-panel`);
+        panel.style.left = "auto";
+        panel.style.right = "-40px";
+        panel.style.transform = "translateX(100%)";
+    }
+
+    var delay = -1.5*Math.random();
     circ.style.animation = `bob 1.5s ease-in-out ${delay}s infinite alternate`;
-
-    document.body.appendChild(circ);
+    
+    if(i == 1) {
+        var p1 = document.querySelector("#p1");
+        var p2 = document.querySelector("#p2");
+        p1.style.animation = `bobTrans 1.5s ease-in-out ${delay}s infinite alternate`;
+        p2.style.animation = `bobTrans 1.5s ease-in-out ${delay}s infinite alternate`;
+    }
 }
